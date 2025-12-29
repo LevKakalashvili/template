1444a8a77918a432e4871876b41c32b1329dee1c
Lev <75985452+LevKakalashvili@users.noreply.github.com>
2025-12-29 15:03:22 +0300

    Add files via upload

---
+++ management/base/command.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from argparse import ArgumentParser, Namespace
+
+from corelib.parser import Parser
+
+
+class AbstractCommand(ABC):
+    @abstractmethod
+    def execute(self): ...
+
+    @abstractmethod
+    def add_arguments(self): ...
+
+    @abstractmethod
+    def set_arguments(self): ...
+
+
+class BaseCommand(AbstractCommand, ABC):
+    args: Namespace = None
+    help: str = None
+
+    def __init__(self, args: list[str], parser: ArgumentParser):
+        self.parser = parser
+        self.add_arguments()
+        self.set_arguments()
+        self.args = self.parser.parse_args(args)
+
+    def set_arguments(self):
+        self.parser.add_help = bool(self.help)
+        parser = Parser()
+        parser.remove_argument("mode")
---
+++ management/seed/constants.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+# Стандартные служебные ключи JSON
+KEY_FIELD = "__key__"
+CHILDREN_FIELD = "__children__"
